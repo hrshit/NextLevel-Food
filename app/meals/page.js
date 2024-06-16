0c9ec508f26ad5ff111bfca5b1dc238695d406c1
Harshit kumar <122025308+hrshit@users.noreply.github.com>
2024-06-16 16:51:48 +0530

    all page update

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,10 +1,35 @@
 
 import Link from "next/link"
+import classes from "./page.module.css"
+import MealsGrid from "@/components/meals/meals-grid"
+import { getMeals } from "@/lib/meals"
+import { Suspense } from "react";
+
+async function Meals(){
+    const meals = await getMeals();
+    return <MealsGrid meals={meals}/>;
+}
+
 export default function MealsPage(){
     return(
         <>
-            <header></header>
-            <main></main>
+            <header className={classes.header}>
+                <h1>
+                    Delecious Meals, created {''}
+                    <span className={classes.highlight}> by you </span>
+                </h1>
+                <p>choose your favorite receipe and cook it yourself. It is easy and fun</p>
+                <p className={classes.cta}>
+                    <Link href="/meals/share">
+                        Share your favorite Receipe
+                    </Link>
+                </p>
+            </header>
+            <main className={classes.main}>
+                <Suspense fallback={ <p className={classes.loading}>Loading Meals....</p> }>
+                    <Meals/>
+                </Suspense>
+            </main>
         </>
     )
 } 
--- components/main-header/main-header-background.js
+++ components/main-header/main-header-background.js
@@ -5,7 +5,7 @@ export default function MainHeaderBackground() {
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
       <defs>
         <linearGradient id="gradient" x1="0%" y1="0%" x2="100%" y2="0%">
-          <stop
+          <stop 
             offset="0%"
             style={{ stopColor: '#59453c', stopOpacity: '1' }}
           />
